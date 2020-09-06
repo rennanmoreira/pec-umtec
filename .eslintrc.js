259b0414b6220da0eceef7ec0165c5a3466e5c7e
Rennan Moreira <rennanmore@gmail.com>
2020-09-06 15:36:52 -0300

    style: eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+	root: true,
+	env: {
+		node: true
+	},
+	'extends': [
+	],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'indent': ['error', 2],
+		'vue': {
+			'script-indent': [
+				'error',
+				2,
+				{ 'baseIndent': 1 }
+			]
+		},
+	},
+
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import router from './router/index.router'
+import store from './store/index.store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
